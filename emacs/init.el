af904a8c09c18182ad470c32ebfe9b0244ff9e2f
bugdie4k <fedorough@gmail.com>
2018-03-27 13:15:41 +0300

    more on vscode

--- emacs/init.el
+++ emacs/init.el
@@ -729,14 +729,14 @@
 
 (use-package web-mode
   :config
-  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))  
   (setq web-mode-enable-auto-pairing t)
   (setq web-mode-enable-auto-closing t)
   (setq web-mode-enable-auto-expanding t)
   (setq web-mode-enable-auto-opening t)
   (setq web-mode-enable-css-colorization t)
   (setq web-mode-enable-current-element-highlight t)
-  (setq web-mode-enable-current-column-highlight t)  )
+  (setq web-mode-enable-current-column-highlight t))
 
 (use-package emmet-mode
   :config
