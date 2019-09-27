fa969bcb2c9434fc5f7f40c2903f5174785f4cb8
Danylo Fedorov <2d4kda@gmail.com>
2019-09-27 03:45:52 +0300

    Adding copy action to lim + tweaks

--- lim.bash
+++ lim.bash
@@ -14,7 +14,8 @@
 # - LIM_ACTION
 
 # Actions are
-# - install
+# - link
+# - copy (not implemented yet)
 # - remove
 # - skip
 
@@ -28,7 +29,7 @@
 # - 10 - Generic error
 # - 20 - Action not performed because of interactive choice
 # - 30 - Action not performed because of pre-existing success state
-# ('already exists' for 'install' action or 'already does not exist' for 'remove')
+# ('already exists' for 'link' action or 'already does not exist' for 'remove')
 
 function echo_nf {
   tput setaf "$1"
@@ -68,7 +69,64 @@ function echo_fb {
   echo
 }
 
-function _lim_install_broken_link {
+function _install_file {
+  local -ri sudo="$1"
+  local -r  action="$2"
+  local -r  target="$3"
+  local -r  link_or_copypath="$4"
+
+  case "$action" in
+    link)
+      if [[ $sudo -eq 1 ]]; then
+        sudo ln -sv "$target" "$link_or_copypath"
+        return $?
+      else
+        ln -sv "$target" "$link_or_copypath"
+        return $?
+      fi
+      ;;
+    copy)
+      if [[ $sudo -eq 1 ]]; then
+        sudo cp -v "$target" "$link_or_copypath"
+        return $?
+      else
+        cp -v "$target" "$link_or_copypath"
+        return $?
+      fi
+      ;;
+    *)
+      return 10
+      ;;
+   esac
+}
+
+function _rm_file {
+  local -ri sudo="$1"
+  local -r  file="$2"
+
+  if [[ $sudo -eq 1 ]]; then
+    sudo rm -v "$file"
+    return $?
+  else
+    rm -v "$file"
+    return $?
+  fi
+}
+
+function _mkdirp {
+  local -ri sudo="$1"
+  local -r  dirname="$2"
+
+  if [[ $sudo -eq 1 ]]; then
+    sudo mkdir -p "$dirname"
+    return $?
+  else
+    mkdir -p "$dirname"
+    return $?
+   fi
+}
+
+function _lim_install_link_broken_link {
   local -r  target="$1"
   local -r  link="$2"
   local -ri sudo="$3"
@@ -78,8 +136,6 @@ function _lim_install_broken_link {
   local -r existing_link_target_basename=$(basename "$existing_link_target")
   local -r target_basename=$(basename "$target")
   if [[ $existing_link_target_basename = "$target_basename" ]]; then
-    shift
-    shift
     echo_f 2 "Broken link points to a path with the same basename as target: $existing_link_target_basename"
     echo "Current target: $existing_link_target"
     echo "New target:     $target"
@@ -88,15 +144,9 @@ function _lim_install_broken_link {
     case "$choice" in
       y|Y)
         echo_f 2 "Changing it!"
-        if [[ $sudo -eq 1 ]]; then
-          rm -v "$link" &&
-            ln -sv "$target" "$link"
-          return $?
-        else
-          sudo rm -v "$link" &&
-            sudo ln -sv "$target" "$link"
-          return $?
-        fi
+        _rm_file "$sudo" "$link" &&
+          _install_file "$sudo" link "$target" "$link"
+        return $?
         ;;
       *)
         echo_f 3 "Do nothing"
@@ -107,27 +157,22 @@ function _lim_install_broken_link {
   return 10
 }
 
-function _lim_install_no_link_dir {
-  local -r  target="$1"
-  local -r  link="$2"
-  local -r  link_dirname="$3"
-  local -ri sudo="$4"
+function _lim_install_no_dir {
+  local -r  action="$1"
+  local -r  target="$2"
+  local -r  link_or_copypath="$3"
+  local -r  dirname="$4"
+  local -ri sudo="$5"
 
-  echo "Dir for a link does not exist: $link_dirname"
+  echo "Dir does not exist: $dirname"
   local choice
   read -r -p 'Create this dir? [y/N] ' choice
   case "$choice" in
     Y|y)
-      echo_f 2 "Creating '$link_dirname'!"
-      if [[ $sudo -eq 1 ]]; then
-        sudo mkdir -p $link_dirname &&
-          sudo ln -sv "$target" "$link"
-        return $?
-      else
-        mkdir -p $link_dirname &&
-          ln -sv "$target" "$link"
-        return $?
-      fi
+      echo_f 2 "Creating '$dirname'!"
+      _mkdirp "$sudo" "$dirname" &&
+        _install_file "$sudo" "$action" "$target" "$link_or_copypath"
+      return $?
       ;;
     *)
       echo_f 3 "Do nothing"
@@ -136,54 +181,48 @@ function _lim_install_no_link_dir {
   esac
 }
 
-function _lim_install_link_doesnt_exist {
-  local -r  target="$1"
-  local -r  link="$2"
-  local -ri sudo="$3"
+function _lim_install_doesnt_exist {
+  local -r  action="$1"
+  local -r  target="$2"
+  local -r  link_or_copypath="$3"
+  local -ri sudo="$4"
 
-  if [[ $sudo -eq 1 ]]; then
-    sudo ln -sv "$target" "$link"
-    return $?
-  else
-    ln -sv "$target" "$link"
-    return $?
-  fi
+  return $?
 }
 
-function _lim_install_link_exists {
-  local -r  target="$1"
-  local -r  link="$2"
-  local -ri sudo="$3"
+function _lim_install_exists {
+  local -r  action="$1"
+  local -r  target="$2"
+  local -r  link_or_copypath="$3"
+  local -ri sudo="$4"
 
-  echo "File exists: $link"
-  if [[ -L "$link" ]]; then
-    local -r existing_link_target=$(readlink "$link")
+  echo "File exists: $link_or_copypath"
+  if [[ -L "$link_or_copypath" ]]; then
+    local -r existing_link_target=$(readlink "$link_or_copypath")
     echo "File is a link to $existing_link_target"
     if [[ "$existing_link_target" = "$target" ]]; then
       echo No need to install, alredy points to target
       return 30
     else
-      echo "Need to manually check the link: $link -> $existing_link_target"
+      echo "Need to manually check the link: $link_or_copypath -> $existing_link_target"
       return 10
     fi
-  elif [[ -f "$link" ]]; then
+  elif [[ -f "$link_or_copypath" ]]; then
     echo File is a regular file
-    if diff "$link" "$target"; then
+    if diff "$link_or_copypath" "$target"; then
       echo_f 2 "But there is no diff!"
+
+      if [[ $action = 'copy' ]]; then
+        return
+      fi
+
       local choice
-      read -r -p 'Remove it and make a link? [y/N] ' choice
+      read -r -p "Remove it and make a link? [y/N] " choice
       case "$choice" in
         Y|y)
           echo_f 2 "Replacing it with a link!"
-          if [[ $sudo -eq 1 ]]; then
-            sudo rm -v "$link" &&
-              sudo ln -sv "$target" "$link"
-            return $?
-          else
-            rm -v "$link" &&
-              ln -sv "$target" "$link"
-            return $?
-          fi
+          _rm_file "$sudo" "$link_or_copypath" &&
+            _install_file "$sudo" link "$target" "$link_or_copypath" 
           ;;
         *)
           echo_f 3 "Do nothing"
@@ -192,80 +231,105 @@ function _lim_install_link_exists {
       esac
     else
       echo_f 1 "There is a diff!"
-      echo "Existing file: $link"
+      echo "Existing file: $link_or_copypath"
       echo "Target:        $target"
       return 10
     fi
   else
-    echo "Unexpected file type, need manual check: $(file $link)"
+    echo "Unexpected file type, need manual check: $(file $link_or_copypath)"
     return 10
   fi
 }
 
-
-function _lim_install {
-  local -r  target="$1"
-  local -r  link="$2"
-  local -ri sudo="$3"
-
-  local -r link_dirname="$(dirname $link)"
-
-  echo -n 'I> '
-  echo_nb 4 "$link"
-  echo -n ' -> '
-  echo_fb 0 12 "$target"
+function _check_target_exists {
+  local -r target="$1"
 
   if [[ ! -e $target ]]; then
     echo "Target does not exist: $target"
     return 10
-  elif [[ ! -e $link ]] && [[ -L $link ]]; then
-    _lim_install_broken_link "$target" "$link" "$sudo"
-    return $?
-  elif [[ ! -e $link_dirname ]]; then
-    _lim_install_no_link_dir "$target" "$link" "$link_dirname" "$sudo"
-    return $?
-  elif [[ ! -e $link ]];  then
-    _lim_install_link_doesnt_exist "$target" "$link" "$sudo"
+  fi
+}
+
+function _is_broken_link {
+  local -r file="$1"
+
+  if [[ ! -e "$file" ]] && [[ -L "$file" ]]; then
+    return 0
   else
-    _lim_install_link_exists "$target" "$link" "$sudo"
+    return 1
+  fi
+}
+
+function _lim_install {
+  local -ri sudo="$1"
+  local -r  action="$2"
+  local -r  target="$3"
+  local -r  link_or_copypath="$4"
+
+  if [[ $action = 'link' ]]; then
+    echo -n 'L> '
+  else
+    echo -n 'C> '
+  fi 
+  echo_nb 4 "$link_or_copypath"
+  echo -n ' -> '
+  echo_fb 0 12 "$target"
+
+  _check_target_exists "$target" || return $?
+
+  local -r link_dirname="$(dirname $link_or_copypath)"
+
+    echo I AM HERER
+  if _is_broken_link "$link_or_copypath"; then
+    if [[ $action = 'link' ]]; then
+      _lim_install_link_broken_link "$target" "$link_or_copypath" "$sudo"
+      return $?
+    else
+      local -r existing_link_target=$(readlink "$link_or_copypath")
+      echo "Broken symbolic link: $link_or_copypath -> $existing_link_target"
+      return 10
+    fi
+  elif [[ ! -e $link_dirname ]]; then
+    _lim_install_no_dir "$action" "$target" "$link_or_copypath" "$link_dirname" "$sudo"
+    return $?
+  elif [[ ! -e $link_or_copypath ]];  then
+    _install_file "$sudo" "$action" "$target" "$link_or_copypath"
+    return $?
+  else
+    _lim_install_exists "$action" "$target" "$link_or_copypath" "$sudo"
     return $?
   fi
 }
 
 function _lim_remove {
-  local -r  target=$1
-  local -r  link="$2"
-  local -ri sudo="$3"
+  local -ri sudo="$1"
+  local -r  target=$2
+  local -r  link_or_copypath="$3"
 
   echo -n 'R> '
-  echo_fb 0 12 "$link"
+  echo_b 4 "$link_or_copypath"
 
-  if [[ ! -e $link ]]; then
-    echo "Doesn't exist: $link"
+  if [[ ! -e $link_or_copypath ]]; then
+    echo "Doesn't exist: $link_or_copypath"
     return 30
   fi
 
-  if [[ ! -L $link ]]; then
-    echo_f 1 "Cannot remove because is not a link: $link"
-    ls -la $link
+  if [[ ! -L $link_or_copypath ]]; then
+    echo_f 1 "Cannot remove because is not a link, but a real file: $link_or_copypath"
+    ls -la "$link_or_copypath"
     return 10
   fi
 
-  if [[ $sudo -eq 1 ]]; then
-    sudo rm -v "$link"
-  else
-    rm -v "$link"
-  fi
+  _rm_file "$sudo" "$link_or_copypath"
   return $?
 }
 
 function _lim_skip {
   local -r  target=$1
-  local -r  link="$2"
-  local -ri sudo="$3"
+  local -r  link_or_copypath="$2"
 
   echo -n 'S> '
-  echo_nfb 0 8 "$link"
+  echo_nfb 0 8 "$link_or_copypath"
   echo -n ' -> '
   echo_fb 0 8 "$target"
   return 0
@@ -273,21 +337,22 @@ function _lim_skip {
 
 # to pass things around when executing entry
 declare    __LIM_ENTRY_TARGET
-declare    __LIM_ENTRY_LINK
+declare    __LIM_ENTRY_LINK_OR_COPYPATH
 declare    __LIM_ENTRY_ACTION
 declare -i __LIM_ENTRY_SUDO
 
 function _lim_parse_input {
-  local name target link action sudo
+  local name target link_or_copypath action sudo
 
   sudo=0
 
   while [[ $# -ne 0 ]]; do
-    case $1 in
-      -NAME) name=$2; shift ;;
-      -TARGET) target=$2; shift ;;
-      -LINK) link=$2; shift ;;
-      -ACTION) action=$2; shift ;;
+    case "$1" in
+      -NAME) name="$2"; shift ;;
+      -TARGET) target="$2"; shift ;;
+      -LINK) link_or_copypath="$2"; shift ;;
+      -COPY) link_or_copypath="$2"; action='copy'; shift ;;
+      -ACTION) action="$2"; shift ;;
       -SUDO) sudo=1 ;;
       *) echo_f 1 "Unknown option: $1"; return 5 ;;
     esac
@@ -299,7 +364,7 @@ function _lim_parse_input {
   fi
 
   if [[ ! -z $LIM_LINKP ]]; then
-    link="${LIM_LINKP%/}/$link"
+    link_or_copypath="${LIM_LINKP%/}/$link_or_copypath"
   fi
 
   if [[ -z $target ]]; then
@@ -307,14 +372,14 @@ function _lim_parse_input {
     return 5
   fi
 
-  if [[ -z $link ]]; then
-    echo_f 1 "Cannot determine a link!"
+  if [[ -z $link_or_copypath ]]; then
+    echo_f 1 "Cannot determine a link or a copypath!"
     return 5
   fi
 
   if [[ ! -z $name ]]; then
     target="${target%/}/$name"
-    link="${link%/}/$name"
+    link_or_copypath="${link_or_copypath%/}/$name"
   fi
 
   if [[ -z $action ]]; then
@@ -326,7 +391,7 @@ function _lim_parse_input {
   fi
 
   __LIM_ENTRY_TARGET=$target
-  __LIM_ENTRY_LINK=$link
+  __LIM_ENTRY_LINK_OR_COPYPATH=$link_or_copypath
   __LIM_ENTRY_ACTION=$action
   __LIM_ENTRY_SUDO=$sudo
 }
@@ -334,20 +399,24 @@ function _lim_parse_input {
 function _lim_perform_action {
   local -r action=$__LIM_ENTRY_ACTION
   local -r target=$__LIM_ENTRY_TARGET
-  local -r link=$__LIM_ENTRY_LINK
+  local -r link_or_copypath=$__LIM_ENTRY_LINK_OR_COPYPATH
   local -r sudo=$__LIM_ENTRY_SUDO
 
   case $action in
-    install)
-      _lim_install "$target" "$link" "$sudo"
+    link)
+      _lim_install "$sudo" link "$target" "$link_or_copypath"
+      return $?
+      ;;
+    copy)
+      _lim_install "$sudo" copy "$target" "$link_or_copypath"
       return $?
       ;;
     remove)
-      _lim_remove "$target" "$link" "$sudo"
+      _lim_remove "$sudo" "$target" "$link_or_copypath" 
       return $?
       ;;
     skip)
-      _lim_skip "$target" "$link" "$sudo"
+      _lim_skip "$target" "$link_or_copypath"
       return $?
       ;;
     *)
@@ -388,24 +457,28 @@ function _lim_report_action {
   fi
 
   case $action in
-    install)
+    link)
       case $retcode in
         0)
-          echo_fb 0 10 "INSTALLED"
+          echo_fb 0 10 "LINK CREATED"
           INSTALLED+=1
           return
           ;;
         20)
-          echo_fb 0 3 "NOT INSTALLED"
+          echo_fb 0 3 "LINK IS NOT CREATED"
           NOT_INSTALLED+=1
           return
           ;;
         30)
-          echo_f 2 "ALREADY EXISTS"
+          echo_f 2 "LINK ALREADY EXISTS"
           EXISTING+=1
           return
           ;;
       esac
+      ;;
+    copy)
+      # TODO: implement
+      return
       ;;
     remove)
       case $retcode in
